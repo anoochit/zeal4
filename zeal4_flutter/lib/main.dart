ee9477e5e133c027fc5eeb0f6081fd45c3cd4ef6
Anuchit Chalothorn <anoochit@gmail.com>
2025-12-08 16:15:09 +0700

    chore(flutter): Update main.dart

--- zeal4_flutter/lib/main.dart
+++ zeal4_flutter/lib/main.dart
@@ -1,9 +1,12 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
 // import 'app/utils/windows_utils.dart';
+import 'app/utils/windows_utils.dart';
 import 'serverpod.dart';
 
 Future<void> main() async {
@@ -30,5 +33,5 @@ Future<void> main() async {
   );
 
   // windows config
-  // initWindows();
+  initWindows();
 }
